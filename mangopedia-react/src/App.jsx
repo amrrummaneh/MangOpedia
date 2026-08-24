d59580e1c3c7506d83f4c82524a508f13421b21b
Amr Rummaneh <amr.rummaneh2@gmail.com>
2026-08-24 21:43:41 +0300

    Footer Component

--- mangopedia-react/src/App.jsx
+++ mangopedia-react/src/App.jsx
@@ -1,11 +1,16 @@
 import AppRoutes from "./routes/AppRouter";
 import Header from "./components/layout/Header";
+import Footer from "./components/layout/Footer";
+
 function App() {
   return (
-    <>
+    <div className="d-flex flex-column min-vh-100 bg-body">
       <Header />
-      <AppRoutes />
-    </>
+      <main className="flex-grow-1">
+        <AppRoutes />
+      </main>
+      <Footer />
+    </div>
   );
 }
 
